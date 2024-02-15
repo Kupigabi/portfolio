0e6b511e9dc4401ccf5cff24bd18d581d82e205c
Kupigabi <gabor.kupeczki@gmail.com>
2024-02-15 15:35:07 +0100

    Home, About added

---
+++ src/components/about/About.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./about.css";
+import AboutImg from "../../assets/MyPhoto.jpg";
+import CV from "../../assets/Kupeczki-CV.pdf";
+import Info from './Info';
+
+function About() {
+  return (
+    <section className="about section" id="about">
+        <h2 className="section__title">About Me</h2>
+        <span className="section__subtitle">My Introduction</span>
+
+        <div className="about__container container grid">
+            <img src={AboutImg} alt="" className="about__img" />
+
+            <div className="about__data">
+                <Info />
+
+                <p className="about__description">I recently finished a junior frontend developertraining, igniting my passion for new technologies.With a creative mindset, I bring fresh ideas toprojects and excel both solo and in teams. Known for adaptability and problem-solving, I strive for excellence, making me a valuable asset. Excited to continue advancing in frontend development,confident in my positive impact on future projects.</p>
+
+                <a download="" href={CV} className="button button--flex">
+                    Download CV
+                  
+<svg width="24px" height="24px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M13.5 3H12H7C5.89543 3 5 3.89543 5 5V19C5 20.1046 5.89543 21 7 21H7.5M13.5 3L19 8.625M13.5 3V7.625C13.5 8.17728 13.9477 8.625 14.5 8.625H19M19 8.625V9.75V12V19C19 20.1046 18.1046 21 17 21H16.5" stroke="var(--container-color)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+<path d="M12 12V20M12 20L9.5 17.5M12 20L14.5 17.5" stroke="var(--container-color)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+</svg>
+                </a>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default About
---
+++ src/components/home/Data.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Data() {
+  return (
+    <div className="home__data">
+        <h1 className="home__title">Gábor Kupeczki</h1>
+        <h3 className="home__subtitle">Visual Designer</h3>
+        <p className="home__description">I am a creative designer based in Hungary, and I am very passionate and dedicated to my work.</p>
+
+        <a href="#contact" className="button button--flex">
+            Say Hello
+        </a>
+    </div>
+  )
+}
+
+export default Data
