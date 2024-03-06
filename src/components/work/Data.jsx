4825a73a1230b1a0ca662272914abfe2e3de56c8
Kupigabi <gabor.kupeczki@gmail.com>
2024-03-06 22:16:32 +0100

    images and urls added

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,35 +1,61 @@
-import Work from "../../assets/work.jpg"
+import Netflix from "../../assets/Netflix.png"
+import Notes from "../../assets/Notes.png"
+import Quiz from "../../assets/Quiz.png"
+import Expense from "../../assets/Expense_tracker.png"
+import Tenzies from "../../assets/Tenzies.png"
+import Todo from "../../assets/Todo_list.png"
+import Weather from "../../assets/Weather.png"
+
 
 export const projectsData = [
     {
         id: 1,
-        image: Work,
+        image: Netflix,
         title: 'Web desing',
+        website:"https://kupigabi.github.io/netflix-deploy/",
         category: 'web'
     },
     {
         id: 2,
-        image: Work,
-        title: 'App movil',
+        image: Notes,
+        title: 'App',
+        website:"https://kupigabi.github.io/notes-app/",
         category: 'app'
     },
     {
         id: 3,
-        image: Work,
-        title: 'Brand desing',
-        category: 'design'
+        image: Quiz,
+        title: 'App',
+        website:"https://kupigabi.github.io/quiz-app/",
+        category: 'app'
     },
     {
         id: 4,
-        image: Work,
-        title: 'App movil',
+        image: Expense,
+        title: 'App',
+        website:"https://kupigabi.github.io/expense-tracker/",
         category: 'app'
     },
     {
         id: 5,
-        image: Work,
-        title: 'Web design',
-        category: 'web'
+        image: Tenzies,
+        title: 'Game',
+        website:"https://kupigabi.github.io/tenzies-game/",
+        category: 'game'
+    },
+    {
+        id: 6,
+        image: Weather,
+        title: 'App',
+        website:"https://kupigabi.github.io/weather-app/",
+        category: 'app'
+    },
+    {
+        id: 7,
+        image: Todo,
+        title: 'App',
+        website:"https://kupigabi.github.io/todo-list/",
+        category: 'app'
     },
 ];
 
